a32017290e2325da6646c01ef51e15108ca17b80
Ming Wu <mingwu1214@gmail.com>
2023-10-24 06:08:56 +0800

    modify json-server name

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ let loginBtn = document.querySelector('.loginBtn');
 // 註冊
 function signUp() {
     if (signUpEmail.value == 'admin@mail.com' && signUpPassword.value == '123123') {
-        axios.post('https://mingwu-json-server.onrender.com/users', {
+        axios.post('https://nini-json-server.onrender.com/users', {
             "email": signUpEmail.value,
             "password": signUpPassword.value,
             "identity": 'admin'
@@ -26,7 +26,7 @@ function signUp() {
                 console.log(error.response);
             })
     } else {
-        axios.post('https://mingwu-json-server.onrender.com/users', {
+        axios.post('https://nini-json-server.onrender.com/users', {
             "email": signUpEmail.value,
             "password": signUpPassword.value
         })
@@ -57,7 +57,7 @@ signUpBtn.addEventListener('click', function () {
 // 登入
 function login() {
     if (loginEmail.value == 'admin@mail.com' && loginPassword.value == '123123') {
-        axios.post('https://mingwu-json-server.onrender.com/login', {
+        axios.post('https://nini-json-server.onrender.com/login', {
             "email": loginEmail.value,
             "password": loginPassword.value,
         })
@@ -71,7 +71,7 @@ function login() {
                 alert.log(error.response);
             })
     } else {
-        axios.post('https://mingwu-json-server.onrender.com/login', {
+        axios.post('https://nini-json-server.onrender.com/login', {
             "email": loginEmail.value,
             "password": loginPassword.value
         })
--- js/logOutSeeMore.js
+++ js/logOutSeeMore.js
@@ -2,7 +2,7 @@ let id = location.href.split('=')[1];
 
 // 渲染db.json
 function showContent() {
-    axios.get(`https://mingwu-json-server.onrender.com/views/${id}`)
+    axios.get(`https://nini-json-server.onrender.com/views/${id}`)
         .then(function (response) {
             data = response.data;
             content.textContent = JSON.stringify(response.data);
